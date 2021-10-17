1e3778b0b483f6fab47b45eef4faef22c3b3333d
TheSlimyOne <70922587+TheSlimyOne@users.noreply.github.com>
2021-10-17 13:47:06 -0400

    Update

--- JS/assignment03.js
+++ JS/assignment03.js
@@ -1,21 +1,15 @@
 let elem = [];
-// assign the entire table row for hole 1 to a variable, elem
-// elem[1] = document.getElementById("1");
-
-// display the number of children (all td elements)
-// console.log(elem.children.length);
-// display the content of the + button, which is the first child of the fifth element
-// console.log(elem.children[4].children[0]); 
-
-// assign a function to the + button
-// elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
 
 for(let i=1; i<=18; i++) {
-  // console.log(i);
-  elem[i] = document.getElementById(i.toString());
-  elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
-  elem[i].children[5].children[0].onclick = function(){sub1(elem[i]);};
-  
+    // console.log(i);
+    elem[i] = document.getElementById(i.toString());
+    elem[i].children[4].children[0].onclick = function(){add1(elem[i]); modifyOver(elem[i]); total(elem[i]);};
+    elem[i].children[4].children[1].onclick = function(){sub1(elem[i]); modifyOver(elem[i]); total(elem[i]);};
+    let warnBtn = document.createElement("button")
+    warnBtn.className = "btn btn-warning"
+    warnBtn.innerHTML = "C"
+    elem[i].children[4].appendChild(warnBtn);
+
 
 }
 
@@ -32,10 +26,42 @@ function add1 (elem) {
 
 // create an "sub1" function
 function sub1 (elem) {
-    if(elem.children[2].innerHTML != "-"){
+    if (elem.children[2].innerHTML != "-" && elem.children[2].innerHTML != "1"){
         let currentScore = elem.children[2].innerHTML;
         currentScore = Number.parseInt(currentScore);
-        elem.children[2].innerHTML = currentScore - 1;  
+        elem.children[2].innerHTML = currentScore - 1;
     }
   }
-  
+
+// Calculating over function
+function modifyOver(elem){
+    if (elem.children[2].innerHTML != "-")
+        elem.children[3].innerHTML = elem.children[1].innerHTML - elem.children[2].innerHTML;
+    else if(elem.children[3].innerHTML = "0"){
+        elem.children[3].innerHTML = "-";
+    }
+}
+
+// Calculating total function
+function total(){
+    let totalPar = 0
+    let totalScore = 0
+    let totalOver = 0
+    
+    for (let i=1; i <= 18; i++){
+        if (elem[i].children[1].innerHTML != "-")
+            totalPar += Number.parseInt(elem[i].children[1].innerHTML);
+        if (elem[i].children[2].innerHTML != "-")
+            totalScore += Number.parseInt(elem[i].children[2].innerHTML);
+        if (elem[i].children[3].innerHTML != "-")
+            totalOver += Number.parseInt(elem[i].children[3].innerHTML);
+        
+    }
+
+    let totalTable = document.getElementById("totals")
+    totalTable.children[1].innerHTML = totalPar;
+    totalTable.children[2].innerHTML = totalScore;
+    totalTable.children[3].innerHTML = totalOver;
+
+
+}
