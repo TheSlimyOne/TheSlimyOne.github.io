4c02b87521bcfa4cc618d101431675f9d95ee48e
TheSlimyOne <70922587+TheSlimyOne@users.noreply.github.com>
2022-06-29 12:26:35 -0400

    Update collegeDebt.js

--- JS/collegeDebt.js
+++ JS/collegeDebt.js
@@ -39,8 +39,6 @@ function loadDoc() {
     $("#loan_year0" + i).prop("disabled", true); // document.getElementById("loan_year0" + i).disabled = true;
     $("#loan_year0" + i).css("background-color", "gray") // document.getElementById("loan_year0" + i).style.backgroundColor = "gray";
     $("#loan_year0" + i).css("color", "white") // document.getElementById("loan_year0" + i).style.color = "white";
-    
-
     $("#loan_amt0" + i).val(defaultLoanAmount.toFixed(2)); // document.getElementById("loan_amt0" + i).value = defaultLoanAmount.toFixed(2);
     $("#loan_int0" + i).val(defaultInterestRate); // document.getElementById("loan_int0" + i).value = defaultInterestRate;
     $("#loan_int0" + i).prop("disabled", true); // document.getElementById("loan_int0" + i).disabled = true;
@@ -62,8 +60,13 @@ function loadDoc() {
 
   // set focus to first year: messes up codepen
   $("#loan_year01").focus();
+
   $("#loan_year01").blur(function () {
-    updateLoansArray();
+    updateYear();
+  });
+
+  $("#loan_int01").blur(function () {
+    updateIntRate();
   });
 
 } // end: function loadDoc()
@@ -73,19 +76,64 @@ function toComma(value) {
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function updateLoansArray() {
-  // YEAR
-  // If the user inputs nothing use the default value
-  ($("#loan_year01").val() == "") ? loans[0].loan_year = parseInt($("#loan_year01").attr('placeholder')) :
-   loans[0].loan_year = parseInt($("#loan_year01").val());
+function updateYear(){
 
-  for (var i = 1; i < 5; i++) {
-    loans[i].loan_year = loans[0].loan_year + i;
-    $("#loan_year0" + (i + 1)).val(loans[i].loan_year);
+  // YEAR
+  yearReg = /^(19|20)\d{2}$/; // only allow numberical characters within the field
+
+
+  if ((yearReg).test($("#loan_year01").val())){
+    loans[0].loan_year = parseInt($("#loan_year01").val())
+
+    for (var i = 1; i < 5; i++) {
+      loans[i].loan_year = loans[0].loan_year + i;
+      $("#loan_year0" + (i + 1)).val(loans[i].loan_year);
+    }
+
+  } else {
+    $("#loan_year01").css("background-color", "red") // document.getElementById("loan_year0" + i).style.backgroundColor = "gray";
+    
   }
 
-  // INTREST RATE
+}
+
+function updateIntRate(){
+  // With any leading number with 
+  intReg = /^([0-9]+?)(\.[0-9]+?)$/;  
   
+  // INTREST RATE
+  if ((intReg).test($("#loan_int01").val())){
+    loans[0].loan_int_rate = parseInt($("#loan_int01").val())
+
+    for (var i = 1; i < 5; i++) {
+      $("#loan_int0" + (i + 1)).val(loans[i].loan_int_rate);
+    }
+
+  } else {
+    $("#loan_int01").css("background-color", "red") // document.getElementById("loan_year0" + i).style.backgroundColor = "gray";
+    
+  }
+
+}
+
+function updateLoansArray() {
+  
+  
+  amtReg = /^\d$/;
+  
+ 
+  
+
+
+
+  //  = manditory digit + an optional decimal and digit
+  // This allows for integers and decals  
+  // if ($("#loan_int01").val().match()){
+  //   loans[0].loan_year = parseInt($("#loan_year01").val());
+  // } else {
+  //   $("loan_year01").val($("#loan_year01").val($("#loan_year01").attr('placeholder')))
+  //   loans[0].loan_year = parseInt($("#loan_year01").attr('placeholder'));
+  // }
 
   // AMOUNT
 
